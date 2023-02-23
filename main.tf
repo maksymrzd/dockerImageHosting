66836f702ba2230c9a75c2545374229fcea3cd9f
maksymrzd <maksym.razdobarov@gmail.com>
2023-02-23 19:32:41 +0200

    edited main.tf file

--- main.tf
+++ main.tf
@@ -11,13 +11,42 @@ resource "aws_instance" "dockerhostinstance" {
   tags = {
     Name = "Docker image hosting"
   }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("aws_key.pem")
+  }
+
+  provisioner "file" {
+    source = "mywebsite.conf"
+    destination = "/home/ec2-user/mywebsite.conf"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install nginx1 -y",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx",
+      "sudo yum install docker -y",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo docker pull maksymrzd/mywebsite:v3",
+      "sudo docker run -d -p 8080:80 maksymrzd/mywebsite:v3",
+      "sudo mv mywebsite.conf /etc/nginx/conf.d/",
+      "sudo nginx -t",
+      "sudo systemctl restart nginx"
+    ]
+    }
 }
 
 resource "aws_security_group" "dockersg" {
   dynamic "ingress" {
-    for_each = ["80", "22"]
+    for_each = ["80", "22", "443"]
     content {
-      description = "HTTP, SSH ports"
+      description = "HTTP, HTTPS, SSH ports"
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
